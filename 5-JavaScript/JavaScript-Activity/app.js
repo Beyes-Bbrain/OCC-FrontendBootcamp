c694ed6c5fc018c207899198b374814b3ab59bb7
Yuanyuan <maliaw0823@gmail.com>
2023-12-07 21:22:32 -0800

    Grade Calculator

---
+++ 5-JavaScript/JavaScript-Activity/app.js
@@ -0,0 +1,26 @@
+let score = parseInt(prompt("Enter the score: "));
+
+//Conditional Statements
+let grade;
+
+//if, else if, else
+if( score >=90){
+
+console.log("Congrats! You got grade A!");
+
+} else if(score >= 80&& score <= 89){
+
+    console.log("Great job!You got grade B!");
+
+} else if(score >= 70&& score <= 79){
+
+    console.log("You got grade C!");
+
+} else if(score >= 60&& score <= 69){
+
+    console.log("You got grade D!");
+
+} else {
+
+    console.log("You got grade F!");
+}
